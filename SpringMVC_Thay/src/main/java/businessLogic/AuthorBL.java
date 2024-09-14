cb7eca6e7045dee99f307a4fb1bc41d9e4222190
Hòa Nguyễn <48762609+student1060632fpt@users.noreply.github.com>
2024-09-14 18:30:59 +0700

    Refactor: Add CRUD operations for Author entity in AuthorBL.java
    This commit adds the CRUD (Create, Read, Update, Delete) operations for the Author entity in the AuthorBL.java file. It includes methods to get an author by ID, add a new author, edit an existing author, and delete an author. The code has been refactored to improve the readability and maintainability of the codebase.

--- SpringMVC_Thay/src/main/java/businessLogic/AuthorBL.java
+++ SpringMVC_Thay/src/main/java/businessLogic/AuthorBL.java
@@ -23,10 +23,52 @@ public class AuthorBL {
 			}
 		});
 	}
+	public static Author getAuthor(int id) {
+		String sql = "SELECT * FROM author WHERE authorid = ?";
+		// đối số id
+		return (Author) jdbc.queryForObject(sql, new RowMapper<Author>() {
+			@Override
+			public Author mapRow(ResultSet rs, int numRow) throws SQLException {
+				Author at = new Author();
+				at.setAuthorId(rs.getInt("authorid"));
+				at.setAuthorName(rs.getString("authorname"));
+				return at;
+			}
+		}, id);
+	}
+	public static int add(Author at) {
+		String sql = "INSERT INTO author(authorname) value (?)";
+		return jdbc.update(sql, at.getAuthorName());
+	}
+	public static int edit(Author at) {
+		String sql = "UPDATE author SET authorname = ? WHERE authorid = ?";
+		return jdbc.update(sql,at.getAuthorName(),at.getAuthorId());
+	}
+	public static int del(int id) {
+		String sql = "DELETE FROM author WHERE authorid = ?";
+		return jdbc.update(sql,id);
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		List<Author> la = getAuthors();
-		la.forEach(at -> System.out.println(at.getAuthorId()+" - "+at.getAuthorName()));
+//		List<Author> la = getAuthors();
+//		la.forEach(at -> System.out.println(at.getAuthorId()+" - "+at.getAuthorName()));
+	
+//		get author 
+//		Author at = getAuthor(2);
+//		System.out.println(at.getAuthorId()+" - "+ at.getAuthorName());
+	
+//		thêm author
+//		Author at = new Author();
+//		at.setAuthorName("aaa");
+//		System.out.println(add(at));
+	
+//		sửa author
+//		Author auSua = getAuthor(107);
+//		auSua.setAuthorName("bb");
+//		System.out.println(edit(auSua));
+		
+//		xóa author
+		System.out.println(del(107));
 	}
 
 }
