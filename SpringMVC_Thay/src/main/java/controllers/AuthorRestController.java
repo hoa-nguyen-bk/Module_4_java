26c5268031b1b73819b99399609fd46954549fce
Hòa Nguyễn <48762609+student1060632fpt@users.noreply.github.com>
2024-09-29 18:55:06 +0700

    Refactor: Add REST API endpoints for retrieving authors and adding new author
    - Added AuthorRestController to handle REST API for retrieving authors
    - Added support for displaying additional attributes in loai-sua.jsp
    - Added getProduct() method in ProductBL.java and updated CategoryController.java and category-menu.jsp
    - Added support for displaying list of products on the index page

--- SpringMVC_Thay/src/main/java/controllers/AuthorRestController.java
+++ SpringMVC_Thay/src/main/java/controllers/AuthorRestController.java
@@ -1,8 +1,14 @@
 package controllers;
 
+import java.util.Hashtable;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import businessLogic.AuthorBL;
@@ -13,5 +19,20 @@ public class AuthorRestController {
 	@GetMapping("/authors_rest")
 	public List<Author> authorRest(){
 		return AuthorBL.getAuthors();
+	}
+	@GetMapping("/authors_rest2")
+	public Map<String, List<Author>> authorRest2(){
+		Map<String, List<Author>> m = new Hashtable<>();
+		m.put("authors", AuthorBL.getAuthors());
+		return m;
+	}
+	@GetMapping("/authors_rest/{id}")
+	public Author detailAuthorRest(@PathVariable (name = "id") int id){
+		Author at = AuthorBL.getAuthor(id);
+		return at;
+	}
+	@PostMapping("/add-author")
+	public int addAuthorRest(@RequestBody Author at){
+		return AuthorBL.add(at);
 	}
 }
